06b02bb9156d5239bc15096d7a465431b7c71d9e
saravanan <s.k.saravanan94@gmail.com>
2023-01-15 20:16:51 +0530

    stylesheet

---
+++ src/component/Stylesheet.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Stylesheet.css'
+import style from './Stylesheet.module.css'
+
+function Stylesheet(props) {
+    return (<div className={style.primary}>Stylesheet</div>)
+}
+
+export default Stylesheet
